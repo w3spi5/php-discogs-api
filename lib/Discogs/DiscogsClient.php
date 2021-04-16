cddcb66278de9d8160f48a5061cae100faaeacdd
calliostro <denis.will@outlook.de>
2021-04-16 14:20:10 +0200

    fix PHPDOC for magic methods

--- lib/Discogs/DiscogsClient.php
+++ lib/Discogs/DiscogsClient.php
@@ -9,32 +9,33 @@
 namespace Discogs;
 
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
+use GuzzleHttp\Command\Result;
 
 /**
- * @method array getArtist(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-artist-get">See Discogs API Documentation</a>
- * @method array getArtistReleases(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-artist-releases-get">See Discogs API Documentation</a>
- * @method array search(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-search-get">See Discogs API Documentation</a>
- * @method array getRelease(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-release-get">See Discogs API Documentation</a>
- * @method array getMaster(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-master-release-get">See Discogs API Documentation</a>
- * @method array getMasterVersions(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-master-release-versions-get">See Discogs API Documentation</a>
- * @method array getLabel(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-label-get">See Discogs API Documentation</a>
- * @method array getLabelReleases(array $parameters) <a href = "https://www.discogs.com/developers/#page:database,header:database-all-label-releases-get">See Discogs API Documentation</a>
- * @method array getOAuthIdentity() <a href = "https://www.discogs.com/developers/#page:user-identity,header:user-identity-identity-get">See Discogs API Documentation</a>
- * @method array getProfile(array $parameters) <a href = "https://www.discogs.com/developers/#page:user-identity,header:user-identity-profile-get">See Discogs API Documentation</a>
- * @method array getInventory(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-inventory-get">See Discogs API Documentation</a>
- * @method array addInventory(array $parameters) <a href = "https://www.discogs.com/developers/#page:inventory-upload,header:inventory-upload-add-inventory-post">See Discogs API Documentation</a>
- * @method array deleteInventory(array $parameters) <a href = "https://www.discogs.com/developers/#page:inventory-upload,header:inventory-upload-delete-inventory-post">See Discogs API Documentation</a>
- * @method array getOrder(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-order-get">See Discogs API Documentation</a>
- * @method array getOrders(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-orders-get">See Discogs API Documentation</a>
- * @method array changeOrder(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-order-post">See Discogs API Documentation</a>
- * @method array getOrderMessages(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-order-messages-get">See Discogs API Documentation</a>
- * @method array addOrderMessage(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-order-messages-post">See Discogs API Documentation</a>
- * @method array createListing(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-new-listing-post">See Discogs API Documentation</a>
- * @method array changeListing(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-listing-post">See Discogs API Documentation</a>
- * @method array deleteListing(array $parameters) <a href = "https://www.discogs.com/developers/#page:marketplace,header:marketplace-listing-delete">See Discogs API Documentation</a>
- * @method array getCollectionFolders(array $parameters) <a href = "https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-get">See Discogs API Documentation</a>
- * @method array getCollectionFolder(array $parameters) <a href = "https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-folder-get">See Discogs API Documentation</a>
- * @method array getCollectionItemsByFolder(array $parameters) <a href = "https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-items-by-folder-get">See Discogs API Documentation</a>
+ * @method Result getArtist(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-artist-get">See Discogs API Documentation</a>
+ * @method Result getArtistReleases(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-artist-releases-get">See Discogs API Documentation</a>
+ * @method Result search(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-search-get">See Discogs API Documentation</a>
+ * @method Result getRelease(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-release-get">See Discogs API Documentation</a>
+ * @method Result getMaster(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-master-release-get">See Discogs API Documentation</a>
+ * @method Result getMasterVersions(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-master-release-versions-get">See Discogs API Documentation</a>
+ * @method Result getLabel(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-label-get">See Discogs API Documentation</a>
+ * @method Result getLabelReleases(array $parameters) <a href="https://www.discogs.com/developers/#page:database,header:database-all-label-releases-get">See Discogs API Documentation</a>
+ * @method Result getOAuthIdentity() <a href="https://www.discogs.com/developers/#page:user-identity,header:user-identity-identity-get">See Discogs API Documentation</a>
+ * @method Result getProfile(array $parameters) <a href="https://www.discogs.com/developers/#page:user-identity,header:user-identity-profile-get">See Discogs API Documentation</a>
+ * @method Result getInventory(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-inventory-get">See Discogs API Documentation</a>
+ * @method Result addInventory(array $parameters) <a href="https://www.discogs.com/developers/#page:inventory-upload,header:inventory-upload-add-inventory-post">See Discogs API Documentation</a>
+ * @method Result deleteInventory(array $parameters) <a href="https://www.discogs.com/developers/#page:inventory-upload,header:inventory-upload-delete-inventory-post">See Discogs API Documentation</a>
+ * @method Result getOrder(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-order-get">See Discogs API Documentation</a>
+ * @method Result getOrders(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-orders-get">See Discogs API Documentation</a>
+ * @method Result changeOrder(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-order-post">See Discogs API Documentation</a>
+ * @method Result getOrderMessages(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-order-messages-get">See Discogs API Documentation</a>
+ * @method Result addOrderMessage(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-list-order-messages-post">See Discogs API Documentation</a>
+ * @method Result createListing(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-new-listing-post">See Discogs API Documentation</a>
+ * @method Result changeListing(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-listing-post">See Discogs API Documentation</a>
+ * @method Result deleteListing(array $parameters) <a href="https://www.discogs.com/developers/#page:marketplace,header:marketplace-listing-delete">See Discogs API Documentation</a>
+ * @method Result getCollectionFolders(array $parameters) <a href="https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-get">See Discogs API Documentation</a>
+ * @method Result getCollectionFolder(array $parameters) <a href="https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-folder-get">See Discogs API Documentation</a>
+ * @method Result getCollectionItemsByFolder(array $parameters) <a href="https://www.discogs.com/developers/#page:user-collection,header:user-collection-collection-items-by-folder-get">See Discogs API Documentation</a>
  */
 class DiscogsClient extends GuzzleClient
 {
